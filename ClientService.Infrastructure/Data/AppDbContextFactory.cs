1c72e3f0da9f96db0c15b4707fa4e5c4e3e39897
AmineChibani <chibaniamine764@gmail.com>
2025-02-13 10:46:37 +0100

    add DbDroitSpeciaux entitie

---
+++ ClientService.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace ClientService.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var config = new Microsoft.Extensions.Configuration.ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) 
+                .AddJsonFile("ClientService.WebAPI/appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseOracle(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options, null, config);
+        }
+    }
+}
