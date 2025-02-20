1abab911453d72b9cc3cdc22ef1c43ca9377b150
Ali Grich <128514098+grichali@users.noreply.github.com>
2025-02-20 14:52:51 +0100

    use of procedure for gettingadresses

--- ClientService.Core/Entities/DbToutesFamillesSansNull.cs
+++ ClientService.Core/Entities/DbToutesFamillesSansNull.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ namespace ClientService.Core.Entities
     public class DbToutesFamillesSansNull
     {
         [Column("ID_FAMILLE")]
+        [Key]
         public int? ID_FAMILLE { get; set; }
 
         [Column("ID_COULEUR")]
