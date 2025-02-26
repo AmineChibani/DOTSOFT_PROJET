c8a0aecd0e385e77a2efc372e7bcdd4f50ad765b
AmineChibani <chibaniamine764@gmail.com>
2025-02-26 12:00:11 +0000

    fix conflit

--- ClientService.WebAPI/Controllers/ClientController.cs
+++ ClientService.WebAPI/Controllers/ClientController.cs
@@ -54,50 +54,50 @@ namespace ClientService.WebAPI.Controllers
             return Ok(clients);
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AddClient([FromBody] AddClientDto newClientDto)
-        {
-            if (!ModelState.IsValid) return BadRequest(ModelState);
+        //[HttpPost]
+        //public async Task<IActionResult> AddClient([FromBody] AddClientDto newClientDto)
+        //{
+        //    if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            //Mapp Dto to  DbClient entity
-            DbClient newClient = new DbClient
-            {
-                Prenom = newClientDto.Prenom,
-                Nom = newClientDto.Nom,
-                Nom2 = newClientDto.Nom2,
-                TitreId = newClientDto.TitreId,
-                BirthdayDate = newClientDto.BirthdayDate,
-                Mail = newClientDto.Mail,
-                Fdate = newClientDto.Fdate,
-                PointsNumber = newClientDto.PointsNumber,
-                IdTypeClient = newClientDto.IdTypeClient,
-                Particulier = newClientDto.Particulier,
-                StructureId = newClientDto.StructureId,
-                PourMailing = newClientDto.PourMailing,
-                Mailing = newClientDto.Mailing,
-                OkPourMailing = newClientDto.OkPourMailing,
-                PremierAchatPlus = newClientDto.PremierAchatPlus,
-                EmployeId = newClientDto.EmployeId,
-                EmployeIdModification = newClientDto.EmployeIdModification,
-                FDateModification = newClientDto.FDateModification,
-                Commentaire = newClientDto.Commentaire,
-                Interets = newClientDto.Interets,
-                CodeExterne = newClientDto.CodeExterne,
-                NumClient = newClientDto.NumClient,
-                OkPourSms = newClientDto.OkPourSms,
-                OkPourSmsAff = newClientDto.OkPourSmsAff,
-                OkPourMailingPartner = newClientDto.OkPourSmsAff,
-                OkPourSmsPartner = newClientDto.OkPourSmsAff,
-                OkPourMailingAff = newClientDto.OkPourSmsAff,
-                Eticket = newClientDto.Eticket,
-                WebType = newClientDto.WebType,
-                RaisonSociale = newClientDto.RaisonSociale,
-                LivrRaisonSociale = newClientDto.LivrRaisonSociale
-            };
+        //    //Mapp Dto to  DbClient entity
+        //    DbClient newClient = new DbClient
+        //    {
+        //        Prenom = newClientDto.Prenom,
+        //        Nom = newClientDto.Nom,
+        //        Nom2 = newClientDto.Nom2,
+        //        TitreId = newClientDto.TitreId,
+        //        BirthdayDate = newClientDto.BirthdayDate,
+        //        Mail = newClientDto.Mail,
+        //        Fdate = newClientDto.Fdate,
+        //        PointsNumber = newClientDto.PointsNumber,
+        //        IdTypeClient = newClientDto.IdTypeClient,
+        //        Particulier = newClientDto.Particulier,
+        //        StructureId = newClientDto.StructureId,
+        //        PourMailing = newClientDto.PourMailing,
+        //        Mailing = newClientDto.Mailing,
+        //        OkPourMailing = newClientDto.OkPourMailing,
+        //        PremierAchatPlus = newClientDto.PremierAchatPlus,
+        //        EmployeId = newClientDto.EmployeId,
+        //        EmployeIdModification = newClientDto.EmployeIdModification,
+        //        FDateModification = newClientDto.FDateModification,
+        //        Commentaire = newClientDto.Commentaire,
+        //        Interets = newClientDto.Interets,
+        //        CodeExterne = newClientDto.CodeExterne,
+        //        NumClient = newClientDto.NumClient,
+        //        OkPourSms = newClientDto.OkPourSms,
+        //        OkPourSmsAff = newClientDto.OkPourSmsAff,
+        //        OkPourMailingPartner = newClientDto.OkPourSmsAff,
+        //        OkPourSmsPartner = newClientDto.OkPourSmsAff,
+        //        OkPourMailingAff = newClientDto.OkPourSmsAff,
+        //        Eticket = newClientDto.Eticket,
+        //        WebType = newClientDto.WebType,
+        //        RaisonSociale = newClientDto.RaisonSociale,
+        //        LivrRaisonSociale = newClientDto.LivrRaisonSociale
+        //    };
 
-            var Client = await _clientService.AddClient(newClient);
-            return CreatedAtAction(nameof(GetClientById), new { id = newClient.Nom }, newClient);
-        }
+        //    var Client = await _clientService.AddClient(newClient);
+        //    return CreatedAtAction(nameof(GetClientById), new { id = newClient.Nom }, newClient);
+        //}
 
         [HttpGet("clients/addresses/{clientId}")]
         public async Task<IActionResult> GetClientAddresses(int clientId)
@@ -174,9 +174,7 @@ namespace ClientService.WebAPI.Controllers
             var result = await _clientService.GetVentesNationalesAsync(request);
             if(!result.IsSuccess)
             {
-                return result.Error!.Contains("not found") ?
-                   NotFound(result.Error) :
-                   BadRequest(result.Error);
+                return BadRequest(result.Error);
             }
             return Ok(result.Value);
         }
@@ -208,10 +206,9 @@ namespace ClientService.WebAPI.Controllers
 
 
         [HttpGet("CommunicationPreferences/{idClient}/{idStructure}")]
-        public async Task<IActionResult> GetClientCommunicationPreferences([FromRoute] int idClient,[FromRoute] int idStructure)
-
-        {
-            var result = await _clientService.GetClientCommunicationPreferencesAsync(idClient, idStructure);
+        public async Task<IActionResult> GetClientCommunicationPreferences([FromRoute] int idClient, [FromRoute] int idStructure)
+        { 
+            var result = await _clientService.GetClientCommunicationPreferencesAsync(idClient,idStructure);
             if (!result.IsSuccess)
             {
                 return result.Error!.Contains("not found") ?
